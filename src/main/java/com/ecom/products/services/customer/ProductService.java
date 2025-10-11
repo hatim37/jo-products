5c8249e753d27ed48a8c214c6bd7b44ff83baebf
unknown <hatim.web.dev@gmail.com>
2025-10-11 18:15:51 +0200

    get products

---
+++ src/main/java/com/ecom/products/services/customer/ProductService.java
@@ -0,0 +1,28 @@
+package com.ecom.products.services.customer;
+
+import com.ecom.products.dto.ProductDto;
+import com.ecom.products.entity.Product;
+import com.ecom.products.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<ProductDto> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return products.stream().map(Product::getDto).collect(Collectors.toList());
+    }
+
+}
